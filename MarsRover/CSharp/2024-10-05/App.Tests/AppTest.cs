f48241e25f73e53e6db6b7c77867da98f31a46e1
JazJax <jasperhpjackson@gmail.com>
2024-10-05 15:52:39 +0100

    2hrs of TDD! Successful

--- MarsRover/CSharp/2024-10-05/App.Tests/AppTest.cs
+++ MarsRover/CSharp/2024-10-05/App.Tests/AppTest.cs
@@ -1,14 +1,124 @@
-using Xunit;
-using App;
+namespace MarsRover.Tests;
 
-namespace App.Tests;
-
-public class AppShould
+public class RoverShould
 {
     [Fact]
-    public void Exist()
+    public void ReceiveCommandsAsAnArray()
     {
-        var app = new App();
-        Assert.Equal(1,1);
+        RoverApi rover = new RoverApi();
+        char[] commands = {'f','b'};
+        rover.Command(commands);
+    }
+
+    [Fact]
+    public void ActOnEachCommandInTurn()
+    {
+        RoverApi rover = new RoverApi();
+        char[] commands = {'f','f'};
+        (int, int) initialPosition = (0,0);
+        (int, int) finalPosition = (2,0);
+
+        Assert.Equal(initialPosition, rover.GetPosition());
+
+        rover.Command(commands);        
+        Assert.Equal(finalPosition, rover.GetPosition());
+    }
+
+    [Fact]
+    public void HaveAPosition()
+    {
+        RoverApi rover = new RoverApi();
+        (int,int) expectedPosition = (0,0);
+        (int, int) result = rover.GetPosition();
+        Assert.Equal(result, expectedPosition);
+    }
+
+    [Fact]
+    public void HaveADirection()
+    {
+        RoverApi rover = new RoverApi();
+        char expectedDirection = 'N';
+        char result = rover.GetDirection();
+        Assert.Equal(result, expectedDirection);
+    }
+
+    [Fact]
+    public void MoveForwards()
+    {
+        RoverApi rover = new RoverApi();
+        (int, int) initialPosition = (0,0);
+        (int, int) finalPosition = (1,0);
+
+        (int,int) result1 = rover.GetPosition();
+        Assert.Equal(initialPosition, result1);
+
+        rover.Command(['f']);
+        (int,int) result2 = rover.GetPosition();
+        Assert.Equal(finalPosition, result2);
+    }
+
+    [Fact]
+    public void MoveBackwards()
+    {
+        RoverApi rover = new RoverApi();
+        (int, int) initialPosition = (0,0);
+        (int, int) finalPosition = (-1,0);
+
+        (int,int) result1 = rover.GetPosition();
+        Assert.Equal(initialPosition, result1);
+
+        rover.Command(['b']);
+        (int,int) result2 = rover.GetPosition();
+        Assert.Equal(finalPosition, result2);
+    }
+
+    [Fact]
+    public void TurnRight()
+    {
+        RoverApi rover = new RoverApi();
+
+        char direction = rover.GetDirection();
+        Assert.Equal('N', direction);
+        
+        rover.Command(['r']);
+        rover.GetDirection();
+        Assert.Equal('E', rover.GetDirection());
+
+        rover.Command(['r']);
+        rover.GetDirection();
+        Assert.Equal('S', rover.GetDirection());
+
+        rover.Command(['r']);
+        rover.GetDirection();
+        Assert.Equal('W', rover.GetDirection());
+
+        rover.Command(['r']);
+        rover.GetDirection();
+        Assert.Equal('N', rover.GetDirection());
+    }
+
+    [Fact]
+    public void TurnLeft()
+    {
+        RoverApi rover = new RoverApi();
+
+        char direction = rover.GetDirection();
+        Assert.Equal('N', direction);
+        
+        rover.Command(['l']);
+        rover.GetDirection();
+        Assert.Equal('W', rover.GetDirection());
+
+        rover.Command(['l']);
+        rover.GetDirection();
+        Assert.Equal('S', rover.GetDirection());
+
+        rover.Command(['l']);
+        rover.GetDirection();
+        Assert.Equal('E', rover.GetDirection());
+
+        rover.Command(['l']);
+        rover.GetDirection();
+        Assert.Equal('N', rover.GetDirection());
     }
 }
--- MarsRover/CSharp/2024-10-05/App/App.cs
+++ MarsRover/CSharp/2024-10-05/App/App.cs
@@ -1,8 +1,80 @@
-﻿namespace App;
+﻿/* - Develop an API that...
+    -  translates the commands to rover instructions 
 
-public class App
+- You are given the initial starting point (x,y)
+- and the direction (N,S,E,W) it is facing.
+
+- The rover receives a character array of commands.
+    - Implement commands that move the rover forward/backward (f,b).
+    - Implement commands that turn the rover left/right (l,r).
+    - Implement wrapping from one edge of the grid to another. (planets are spheres after all)
+
+- Implement obstacle detection before each move to a new square. If a given sequence of commands encounters an obstacle, the rover moves up to the last possible point, aborts the sequence and reports the obstacle.
+
+## Rules
+- Be careful about edge cases and exceptions. We can not afford to lose a mars rover, just because the developers overlooked a null pointer.
+*/
+
+//QUESTIONS: confirm stating position? Gonna pass in to the constructor to avoid magic strings
+
+//Assunmpotions: North is increase (X,  ); East is increase (  , Y)
+
+namespace MarsRover;
+
+public class RoverApi
 {
-    // public App() {
-    //     throw new NotImplementedException();
-    // }
+    private int north = 0;
+    private int east = 0;
+    private char direction { get; set; } = 'N';
+
+    public void Command(char[] commands)
+    {
+        foreach (char command in commands)
+        {
+            var result = command switch
+            {
+                'f' => north++,
+                'b' => north--,
+                'r' => TurnRight(),
+                'l' => TurnLeft(),
+                _ => 0
+            };
+        }
+    }
+
+    public (int, int) GetPosition()
+    {
+        return (north, east);
+    }
+
+    public char GetDirection()
+    {
+        return direction;
+    }
+
+    private char TurnRight()
+    {
+        direction = direction switch
+        {
+            'N' => 'E',
+            'E' => 'S',
+            'S' => 'W',
+            'W' => 'N'
+        };
+
+        return direction;
+    }
+
+    private char TurnLeft()
+    {
+        direction = direction switch
+        {
+            'N' => 'W',
+            'W' => 'S',
+            'S' => 'E',
+            'E' => 'N'
+        };
+
+        return direction;
+    }
 }
