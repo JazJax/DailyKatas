aa974cf0bb78194b1feaab4db4dc7a8806cc4391
Jasper <jasper.jackson@madetech.com>
2024-10-06 17:14:19 +0100

    Basic video kata

--- VideoStore/CSharp/2024-10-6/App.Tests/AppTest.cs
+++ VideoStore/CSharp/2024-10-6/App.Tests/AppTest.cs
@@ -3,12 +3,49 @@ using App;
 
 namespace App.Tests;
 
-public class AppShould
+public class RentalServiceShould
 {
+    RentalService app;
+    
+    public RentalServiceShould()
+    {
+        app = new RentalService();
+    }
+    
     [Fact]
     public void Exist()
     {
-        var app = new App();
         Assert.Equal(1,1);
+    }
+
+    [Fact]
+    public void AllowCustomerToRentAFilm()
+    {
+        string customer = "Jasper";
+        bool result = app.RentFilm(customer,"The War of the Worlds");
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void NotAllowFilmToBeRentedTwice()
+    {
+        string customer = "Jasper";
+        string filmTitle = "The War of the Worlds";
+        bool result1 = app.RentFilm(customer, filmTitle);
+        Assert.True(result1);
+
+        bool result2 = app.RentFilm(customer, filmTitle);
+        Assert.False(result2);
+    }
+
+    [Fact]
+    public void NotAllowCustomerToRentTwoFilms()
+    {
+        string customer = "Jasper";
+        bool result1 = app.RentFilm("The War of the Worlds", customer);
+        Assert.True(result1);
+        
+        bool result2 = app.RentFilm("Hollow Man", customer);
+        Assert.False(result2);
     }
 }
--- VideoStore/CSharp/2024-10-6/App/App.cs
+++ VideoStore/CSharp/2024-10-6/App/App.cs
@@ -1,8 +1,34 @@
-﻿namespace App;
+﻿using System.Linq;
 
-public class App
+namespace App;
+
+/*
+Video rental
+- Each person can only rent one video
+- Each video can only be rented by one person
+*/
+
+public class RentalService
 {
+    private Dictionary<string, string> rentedFilms = new();
+    
     // public App() {
     //     throw new NotImplementedException();
     // }
+
+    public bool RentFilm(string filmTitle, string customer)
+    {
+        if (rentedFilms.ContainsKey(filmTitle) == true)
+        {
+            return false;
+        }
+
+        if (rentedFilms.Any(x => x.Value == customer) == true)
+        {
+            return false;
+        }
+
+        rentedFilms.Add(filmTitle, customer);
+        return true;
+    }
 }
