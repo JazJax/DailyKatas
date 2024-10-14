99e7e1ed628355bb66f1afa8d2a73d4bffe8a13c
Jasper <54336703+JazJax@users.noreply.github.com>
2024-10-14 10:49:19 +0100

    OO approach - started to run into some missing values as my timer ran out. WIP

--- Tennis/Typescript/2024-10-14/app/app.ts
+++ Tennis/Typescript/2024-10-14/app/app.ts
@@ -1,1 +1,27 @@
-export class App {}
+class Game {
+    player1: Player;
+    player2: Player;
+    score: String = this.getScore();
+
+    constructor(player1: String = 'Unknown player', player2: String = 'Unknown player') {
+        this.player1 = new Player(player1);
+        this.player2 = new Player(player2);
+        // this.score = "Player 1: love \nPlayer 2: love";
+    }
+
+    getScore() {
+        const myPlayer1 = this.player1;
+        return `${this.player1.name}: ${this.player1.score} \n${this.player2.name}: ${this.player2.score}`;
+    }
+}
+
+class Player {
+    name: String;
+    score: String = "love";
+
+    constructor(playerName: String) {
+        this.name = playerName;
+    }
+}
+
+export {Game, Player}
--- Tennis/Typescript/2024-10-14/tests/app.spec.ts
+++ Tennis/Typescript/2024-10-14/tests/app.spec.ts
@@ -1,7 +1,42 @@
-import { App } from '@/app'
+import { Game, Player } from "@/app"
 
-describe('App', () => {
+function constructScoreString(player1, score1, player2, score2) {
+  return `${player1}: ${score1} \n${player2}: ${score2}`
+}
+
+describe('Game', () => {
   it('should exist', () => {
-    expect(new App()).not.toBeNull
+    expect(new Game()).not.toBeNull
+  });
+
+  it('should have two players', () => {
+    const game = new Game();
+    expect(game.player1).not.toBeNull;
+    expect(game.player2).not.toBeNull;
+  })
+
+  it('should have an initial score of love : love', () => {
+    const game = new Game();
+    const expectedScoreString = constructScoreString("Unknown player", "love", "Unknown player", "love")
+    expect(game.score).toBe(expectedScoreString);
+  })
+
+  it('should reflect player names in the score', () => {
+    const game = new Game("Alice", "Bob");
+    const expectedScoreString = constructScoreString("Alice", "love", "Bob", "love")
+    expect(game.score).toBe(expectedScoreString);
+  })
+});
+
+describe('Player', () => {
+  it('should exist', () => {
+    expect(new Player('Test name')).not.toBeNull
+  });
+
+  it('should have a name', () => {
+    const player = new Player('Test name');
+    expect(player.name).not.toBeNull;
   });
 });
+
+// this time, let's go OO
